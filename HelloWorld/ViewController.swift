492af305cafdf23b74e3a3e7b036f016a27a1f29
chika1230 <leon.oneone1@gmail.com>
2021-03-07 14:31:17 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 千頭雅史 on 2021/03/07.
+//  Copyright © 2021 chika1230. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
